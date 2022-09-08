9c8353b96ef2955197efc006414bc66373f0dff8
byeonhyeok <bhrevol@gmail.com>
2022-09-08 22:08:09 +0900

    crypto: add ctcrypt aes-128

---
+++ src/dlsite_utils/crypto.py
@@ -0,0 +1,80 @@
+"""Crytography module."""
+from typing import Optional
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.padding import PKCS7, PaddingContext
+
+
+class CTCryptAES:
+    """CypherTec AES encryption class.
+
+    Args:
+        key: AES key.
+
+    Note:
+        ctcrypt uses AES128 in CBC mode, with W3C padding applied to make any
+        input data align to AES128 block sizes. Padding is always stripped
+        before returning, so the padding method is not actually important.
+    """
+
+    def __init__(self, key: bytes):
+        self.key = key
+        self.algo = algorithms.AES(key)
+        self._pad = PKCS7(self.algo.block_size)
+
+    def _padder(self) -> PaddingContext:
+        return self._pad.padder()
+
+    def encrypt(
+        self,
+        data: bytes,
+        iv: Optional[bytes] = None,
+    ) -> bytes:
+        """CBC encrypt data.
+
+        Args:
+            data: Plaintext data.
+            iv: CBC initialization vector.
+
+        Returns:
+            Encrypted ciphertext.
+        """
+        if iv is None:  # pragma: no cover
+            iv = bytes([0] * 16)
+
+        padder = self._padder()
+        padded_data = padder.update(data)
+        padded_data += padder.finalize()
+        padding = len(padded_data) - len(data)
+
+        cipher = Cipher(self.algo, modes.CBC(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(padded_data)
+        ciphertext += encryptor.finalize()
+        return ciphertext[:-padding]
+
+    def decrypt(self, data: bytes, iv: Optional[bytes] = None) -> bytes:
+        """CBC decrypt data.
+
+        Args:
+            data: Ciphertext data.
+            iv: CBC initialization vector.
+
+        Returns:
+            Decrypted plaintext.
+        """
+        if iv is None:  # pragma: no cover
+            iv = bytes([0] * 16)
+
+        padder = self._padder()
+        padded_data = padder.update(data)
+        padded_data += padder.finalize()
+        padding = len(padded_data) - len(data)
+
+        cipher = Cipher(self.algo, modes.CBC(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        plaintext = decryptor.update(padded_data)
+        plaintext += decryptor.finalize()
+
+        return plaintext[:-padding]
